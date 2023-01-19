261066d0383d63fd6db2be90b3c1a4824a143968
AntoineJr <antoine2104@hotmail.com>
2023-01-19 10:11:25 +0100

    after goddpractice and check_for_cran()

--- R/met.df.R
+++ R/met.df.R
@@ -4,12 +4,12 @@
 #'
 #' @usage data(met.df)
 #'
-#' @format an object of class data.frame with 2 simluated metabolites data
+#' @format an object of class data.frame with 2 simulated metabolites data
 #'
 #'
 #'\describe{ In this dataset we are giving two metabolites data that we
 #'generated ourselves. "met1" is just some white noise from a normal
-#'distribution and "met2" is a metaboite with jumps and trends to correct}
+#'distribution and "met2" is a metabolite with jumps and trends to correct}
 #'
 #' @keywords dataset
 #'
